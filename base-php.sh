adfd31d6a179d15edb973dfa0187f09793676e29
Chingis Sandanov <chingis@wodby.com>
2018-07-31 18:08:52 +0700

    Fixes

--- base-php.sh
+++ base-php.sh
@@ -6,8 +6,8 @@ set -ex
 
 apk add --update git gawk coreutils jq
 
-git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/wodby/base-php"
-cd base-php
+git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/wodby/base-php" /tmp/base-php
+cd /tmp/base-php
 git remote add upstream https://github.com/docker-library/php
 git fetch upstream
 git merge --strategy-option ours upstream/master
