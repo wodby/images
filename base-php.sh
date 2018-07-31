506557dc0a40969afb89b2f03753b3827690be32
Chingis Sandanov <chingis@wodby.com>
2018-07-31 17:49:21 +0700

    Initial release

---
+++ base-php.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+./git.sh
+
+git clone --depth=10 "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/wodby/base-php"
+git remote add upstream https://github.com/docker-library/php
+git fetch upstream
+git merge --strategy-option ours upstream/master
+
+./wodby-meta-update.sh
+
+git_commit ./ "Merge changes from upstream"
+
+git push origin
