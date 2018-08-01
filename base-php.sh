b6898e338a3e0d6e701ee6ceeec658722afe92f4
Chingis Sandanov <chingis@wodby.com>
2018-08-01 14:06:43 +0700

    Add auto update for base-python

--- base-php.sh
+++ base-php.sh
@@ -8,7 +8,7 @@ git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@githu
 cd /tmp/base-php
 git remote add upstream https://github.com/docker-library/php
 git fetch upstream
-git merge --strategy-option ours upstream/master
+git merge --strategy-option ours --no-edit upstream/master
 
 ./wodby-meta-update.sh
 
