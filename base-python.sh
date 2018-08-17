53ccae79dfa2dd971733356d48a6dd792ce9a028
Chingis Sandanov <chingis@wodby.com>
2018-08-17 17:33:03 +0700

    Fixes

--- base-python.sh
+++ base-python.sh
@@ -4,14 +4,4 @@ set -e
 
 . lib.sh
 
-git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/wodby/base-python" /tmp/base-python
-cd /tmp/base-python
-git remote add upstream https://github.com/docker-library/python
-git fetch upstream
-git merge --strategy-option ours --no-edit upstream/master
-
-./wodby-meta-update.sh
-
-git_commit ./ "Merge changes from upstream"
-
-git push origin
+sync_fork "wodby/base-python" "python"
