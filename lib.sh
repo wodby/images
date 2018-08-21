daa813d821ed51713f87b390c06d8cf0474fa0bd
Chingis Sandanov <chingis@wodby.com>
2018-08-21 18:56:11 +0700

    Fixes

--- lib.sh
+++ lib.sh
@@ -6,24 +6,7 @@ set -e
 git config --global user.email "${GIT_USER_EMAIL}"
 git config --global user.name "Wodby Robot"
 
-sync_fork()
-{
-    local repo=$1
-    local upstream=$2
-
-    git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/${repo}" "/tmp/${repo#*/}"
-    cd "/tmp/${repo#*/}"
-    git remote add upstream "https://github.com/docker-library/${upstream}"
-    git fetch upstream
-    git merge --strategy-option ours --no-edit upstream/master
-
-    ./wodby-meta-update.sh
-
-    git_commit ./ "Update from upstream"
-    git push origin
-}
-
-git_commit()
+_git_commit()
 {
     local dir="${1}"
     local msg="${2}"
@@ -38,9 +21,9 @@ git_commit()
     fi
 }
 
-get_image_tags()
+_get_image_tags()
 {
-    local repo=$1
+    local repo="${1}"
 
     wget -q "https://registry.hub.docker.com/v1/repositories/${repo}/tags" -O - \
         | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' \
@@ -48,10 +31,10 @@ get_image_tags()
         | awk -F: '{print $3}'
 }
 
-get_timestamp()
+_get_timestamp()
 {
-    local repo=$1
-    local tag=$2
+    local repo="${1}"
+    local tag="${2}"
 
     if [[ ! "${repo}" =~ / ]]; then
         repo="library/${repo}"
@@ -60,26 +43,28 @@ get_timestamp()
     curl -L -s "https://registry.hub.docker.com/v2/repositories/${repo}/tags/${tag}" | jq -r '.last_updated'
 }
 
-join_ws()
+_join_ws()
 {
     local IFS=
     local s="${*/#/$1}"
     echo "${s#"$1$1$1"}"
 }
 
-release_tag()
+_release_tag()
 {
-    message=$1
-    minor_update=$2
+    local message="${1}"
+    local minor_update="${2}"
 
-    cur_tag=$(git describe --abbrev=0 --tags)
+    local cur_tag=$(git describe --abbrev=0 --tags)
+    local patch_ver
+    local tag
 
     # Patch version changed.
     if [[ -n "${minor_update}" ]]; then
         patch_ver="${cur_tag##*.}"
-        ver="${cur_tag%.*}"
-        minor_ver="${ver#*.}"
-        major_ver="${ver%.*}"
+        local ver="${cur_tag%.*}"
+        local minor_ver="${ver#*.}"
+        local major_ver="${ver%.*}"
         minor_ver=$((minor_ver + 1))
         tag="${major_ver}.${minor_ver}.${patch_ver}"
     # Minor version changed.
@@ -93,19 +78,11 @@ release_tag()
     git push origin "${tag}"
 }
 
-get_base_image()
+_get_dir()
 {
-    local path=$(find . -name Dockerfile -maxdepth 2 | head -n 1)
-    local base_image=""
-
-    base_image=$(grep -oP "(?<=FROM ).+(?=:)" "${path}")
-
-    echo "${base_image}"
-}
-
-get_dir()
-{
-    version=$1
+    local version="${1}"
+    
+    local dir
 
     if [[ -f Dockerfile ]]; then
         dir="."
@@ -120,7 +97,7 @@ get_dir()
     echo "${dir}"
 }
 
-get_suffix()
+_get_suffix()
 {
     local path=$(find . -name Makefile -maxdepth 2 | head -n 1)
     local suffix="(?=$)"
@@ -132,17 +109,17 @@ get_suffix()
     echo "${suffix}"
 }
 
-github_get_latest_ver()
+_github_get_latest_ver()
 {
-    version=$1
-    slug=$2
+    local version="${1}"
+    local slug="${2}"
 
-    url="https://api.github.com/repos/${slug}/git/refs/tags"
-    user="${GITHUB_MACHINE_USER_API_TOKEN}:x-oauth-basic"
-    expr=".[] | select ( .ref | ltrimstr(\"refs/tags/\") | ltrimstr(\"v\") | startswith(\"${version}\")).ref"
+    local url="https://api.github.com/repos/${slug}/git/refs/tags"
+    local user="${GITHUB_MACHINE_USER_API_TOKEN}:x-oauth-basic"
+    local expr=".[] | select ( .ref | ltrimstr(\"refs/tags/\") | ltrimstr(\"v\") | startswith(\"${version}\")).ref"
 
     # Only stable versions.
-    versions=($(curl -s -u "${user}" "${url}" | jq -r "${expr}" | sed -E "s/refs\/tags\/v?//" | grep -oP "^[0-9\.]+$" | sort -rV))
+    local versions=($(curl -s -u "${user}" "${url}" | jq -r "${expr}" | sed -E "s/refs\/tags\/v?//" | grep -oP "^[0-9\.]+$" | sort -rV))
 
     if [[ "${#versions}" == 0 ]]; then
         >&2 echo "Couldn't find latest version in line ${version} of ${slug}."
@@ -152,19 +129,19 @@ github_get_latest_ver()
     fi
 }
 
-get_latest_version()
+_get_latest_version()
 {
-    upstream=$1
-    version=$2
+    local upstream="${1}"
+    local version="${2}"
 
-    local suffix=$(get_suffix)
+    local suffix=$(_get_suffix)
 
     # Get latest stable version from github.
     if [[ "${upstream}" == "github.com"* ]]; then
-        latest_ver=$(github_get_latest_ver "${version}" "${upstream/github.com\//}")
+        latest_ver=$(_github_get_latest_ver "${version}" "${upstream/github.com\//}")
     # From docker hub, only patch updates.
     else
-        local base_image_tags=($(get_image_tags "${upstream}" | grep -oP "^(${version//\./\\.}\.[0-9]+)${suffix}" | sort -rV))
+        local base_image_tags=($(_get_image_tags "${upstream}" | grep -oP "^(${version//\./\\.}\.[0-9\.]+)${suffix}" | sort -rV))
         latest_ver="${base_image_tags[0]}"
     fi
 
@@ -176,17 +153,43 @@ get_latest_version()
     echo "${latest_ver}"
 }
 
+get_base_image()
+{
+    local path=$(find . -name Dockerfile -maxdepth 2 | head -n 1)
+    local base_image=""
+
+    grep -oP "(?<=FROM ).+(?=:)" "${path}"
+}
+
+sync_fork()
+{
+    local repo="${1}"
+    local upstream="${2}"
+
+    git clone "https://${GITHUB_MACHINE_USER}:${GITHUB_MACHINE_USER_API_TOKEN}@github.com/${repo}" "/tmp/${repo#*/}"
+    cd "/tmp/${repo#*/}"
+    git remote add upstream "https://github.com/docker-library/${upstream}"
+    git fetch upstream
+    git merge --strategy-option ours --no-edit upstream/master
+
+    ./wodby-meta-update.sh
+
+    _git_commit ./ "Update from upstream"
+    git push origin
+}
+
 update_versions()
 {
-    local versions=$1
-    local upstream=$2
-    local name=$3
-    local subdir=$4
+    local versions="${1}"
+    local upstream="${2}"
+    local name="${3}"
+    local subdir="${4}"
 
     local updated=()
     local latest_ver
     local latest_timestamp
     local cur_ver
+    local dir
 
     IFS=' ' read -r -a arr_versions <<< "${versions}"
 
@@ -198,17 +201,28 @@ update_versions()
         if [[ -n "${subdir}" ]]; then
             dir="${subdir}"
         else
-            dir=$(get_dir "${version}")
+            dir=$(_get_dir "${version}")
         fi
 
-        cur_ver=$(grep -oP "(?<=${name^^}_VER \?= ).+?$" "${dir}/Makefile")
+        if [[ -f .circleci/config.yml ]]; then
+            cur_ver=$(grep -oP "(?<=${name^^}_VER: )${version//\./\\.}\.[0-9\.]+")
+        else
+            # There two ways how we specify versions in .travis.yml (same used for updates below):
+            # 1. PHP72=7.2.8
+            # 2. PHP_VER=7.2.8
+            cur_ver=$(grep -oP "(?<=${name^^}${version//.}=)[0-9\.]+")
+
+            if [[ -z "${cur_ver}" ]]; then
+                cur_ver=$(grep -oP "(?<=${name^^}_VER=)${version//\./\\.}[0-9\.]+")
+            fi
+        fi
 
         if [[ -z "${cur_ver}" ]]; then
             >&2 echo "Couldn't get the current version of ${version}! Probably need to update the list of supported versions!"
             exit 1
         fi
 
-        latest_ver=$(get_latest_version "${upstream}" "${version}")
+        latest_ver=$(_get_latest_version "${upstream}" "${version}")
 
         if [[ $(compare_semver "${latest_ver}" "${cur_ver}") == 0 ]]; then
             echo "${name^} ${cur_ver} is outdated, updating to ${latest_ver}"
@@ -216,22 +230,19 @@ update_versions()
             if [[ -f .circleci/config.yml ]]; then
                 sed -i -E "s/(${name^^}_VER): ${version//\./\\.}.*/\1: ${latest_ver}/" .circleci/config.yml
             else
-                # There two ways how we specify versions in .travis.yml:
-                # 1. PHP72=7.2.8
-                # 2. PHP_VER=7.2.8
                 sed -i -E "s/(${name^^}${version//.})=.+/\1=${latest_ver}/" .travis.yml
-                sed -i -E "s/(${name^^}_VER)=${version//\./\\.}\.[0-9]+/\1=${latest_ver}/" .travis.yml
+                sed -i -E "s/(${name^^}_VER)=${version//\./\\.}\.[0-9\.]+/\1=${latest_ver}/" .travis.yml
             fi
 
             sed -i -E "s/(${name^^}_VER \?= )${cur_ver}/\1${latest_ver}/" "${dir}/Makefile"
 
             # Update base image timestamps.
             if [[ -f ".${upstream#*/}" ]]; then
-                latest_timestamp=$(get_timestamp "${upstream}" "${cur_ver}")
+                latest_timestamp=$(_get_timestamp "${upstream}" "${cur_ver}")
                 sed -i "s/${cur_ver}#.*/${latest_ver}#${latest_timestamp}/" ".${upstream#*/}"
             fi
 
-            git_commit ./ "Update ${name^} to ${latest_ver}"
+            _git_commit ./ "Update ${name^} to ${latest_ver}"
             updated+=("${latest_ver}")
         else
             echo "Version ${cur_ver} is already the latest version"
@@ -241,15 +252,15 @@ update_versions()
     git push origin
 
     if [[ "${#updated[@]}" != 0 ]]; then
-        ver=$(join_ws ", " "${updated[@]}")
-        release_tag "${name^} updated to ${ver}"
+        ver=$(_join_ws ", " "${updated[@]}")
+        _release_tag "${name^} updated to ${ver}"
     fi
 }
 
 update_timestamps()
 {
-    local versions=$1
-    local base_image=$2
+    local versions="${1}"
+    local base_image="${2}"
     local updated=""
 
     local latest_timestamp
@@ -262,7 +273,7 @@ update_timestamps()
     echo "=============================="
 
     for version in "${arr_versions[@]}"; do
-        latest_timestamp=$(get_timestamp "${base_image}" "${version}")
+        latest_timestamp=$(_get_timestamp "${base_image}" "${version}")
         cur_timestamp=$(cat ".${base_image#*/}" | grep "^${version}" | grep -oP "(?<=#)(.+)$")
 
         if [[ "${cur_timestamp}" != "${latest_timestamp}" ]]; then
@@ -273,7 +284,7 @@ update_timestamps()
     done
 
     if [[ -n "${updated}" ]]; then
-        git_commit ./ "Rebuild against updated base image"
+        _git_commit ./ "Rebuild against updated base image"
         git push origin
     else
         echo "Base image hasn't changed"
@@ -282,9 +293,9 @@ update_timestamps()
 
 update_stability_tag()
 {
-    local version=$1
-    local base_image=$2
-    local branch=$3
+    local version="${1}"
+    local base_image="${2}"
+    local branch="${3}"
     local tag=""
     local minor_update=""
 
@@ -295,13 +306,13 @@ update_stability_tag()
     git checkout "${branch}"
     git merge --no-edit master
 
-    local base_image_tags=($(get_image_tags "${base_image}" | grep -oP "(?<=${version//\./\\.}-)([0-9]\.){2}[0-9]$" | sort -rV))
+    local base_image_tags=($(_get_image_tags "${base_image}" | grep -oP "(?<=${version//\./\\.}-)[0-9\.]+$" | sort -rV))
     local latest_base_image_tag="${base_image_tags[0]}"
-    local cur_base_image_tag=$(grep -oP "(?<=BASE_IMAGE_STABILITY_TAG=)([0-9]\.){2}[0-9]$" .travis.yml)
+    local cur_base_image_tag=$(grep -oP "(?<=BASE_IMAGE_STABILITY_TAG=)[0-9\.]+$" .travis.yml)
 
     if [[ $(compare_semver "${latest_base_image_tag}" "${cur_base_image_tag}") == 0 ]]; then
         sed -i -E "s/(BASE_IMAGE_STABILITY_TAG=)${cur_base_image_tag}/\1${latest_base_image_tag}/" .travis.yml
-        git_commit ./ "Update base image stability tag to ${latest_base_image_tag}"
+        _git_commit ./ "Update base image stability tag to ${latest_base_image_tag}"
         tag=1
     else
         echo "Base image stability tag ${cur_base_image_tag} is already the latest"
@@ -314,6 +325,6 @@ update_stability_tag()
             minor_update=1
         fi
 
-        release_tag "Base image updated to ${latest_base_image_tag}" "${minor_update}"
+        _release_tag "Base image updated to ${latest_base_image_tag}" "${minor_update}"
     fi
 }
