1d74c65349df2853d1d012bdbf5493da7ebdac67
Chingis Sandanov <chingis@wodby.com>
2018-08-21 18:56:11 +0700

    Fixes

--- update-image.sh
+++ update-image.sh
@@ -7,15 +7,15 @@ set -e
 user="${GITHUB_MACHINE_USER}"
 token="${GITHUB_MACHINE_USER_API_TOKEN}"
 
-image=$1
-versions=$2
+image="${1}"
+versions="${2}"
 # Some images have [version].x branches with base image stability tags.
-branch=$3
-# May be a docker image different from base image or github repo.
-upstream=$4
+branch="${3}"
+# Upstream is not a docker image.
+upstream="${4}"
 # When we need to update lang version for vanilla image, e.g. update PHP for vanilla drupal
 # vanilla version dir doesn't match lang version, so we should specify a concrete directory.
-subdir=$5
+subdir="${5}"
 
 IFS=' ' read -r -a array <<< "${versions}"
 
@@ -28,7 +28,7 @@ if [[ -z "${upstream}" ]]; then
     upstream=$(get_base_image)
 fi
 
-# Those with branches update via stability tags.
+# If branch specified image updated via stability tags instead of versions.
 if [[ -z "${branch}" ]]; then
     update_versions "${versions}" "${upstream}" "${image#*/}" "${subdir}"
 fi
