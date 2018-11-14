5526c4d9b433d2d40f3a9d1ed036042d6a3cc897
Chingis Sandanov <chingis@wodby.com>
2018-11-14 10:40:36 +0700

    Temporary disable mariadb 10.1 auto-update https://github.com/wodby/mariadb/issues/10

--- upstream/mariadb.sh
+++ upstream/mariadb.sh
@@ -4,4 +4,5 @@ set -e
 
 . ../update.sh
 
-update_from_upstream "wodby/mariadb" "10.3 10.2 10.1" "mariadb"
+#update_from_upstream "wodby/mariadb" "10.3 10.2 10.1" "mariadb"
+update_from_upstream "wodby/mariadb" "10.3 10.2" "mariadb"
