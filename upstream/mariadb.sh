6e3cc3b95a6c96c735d5fbe50f52ba4b71d7bfe6
Chingis Sandanov <chingis@wodby.com>
2019-02-12 13:11:07 +0700

    Enable back auto updates for MariaDB 10.1 // Delete Ruby 2.3

--- upstream/mariadb.sh
+++ upstream/mariadb.sh
@@ -4,5 +4,4 @@ set -e
 
 . ../update.sh
 
-#update_from_upstream "wodby/mariadb" "10.3 10.2 10.1" "mariadb"
-update_from_upstream "wodby/mariadb" "10.3 10.2" "mariadb"
+update_from_upstream "wodby/mariadb" "10.3 10.2 10.1" "mariadb"
