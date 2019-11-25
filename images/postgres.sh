b4f81962980bac91981f593fa378712c271a9da4
Chingis Sandanov <chingis@wodby.com>
2019-11-25 17:38:42 +0700

    Add postgres 12

--- images/postgres.sh
+++ images/postgres.sh
@@ -5,4 +5,4 @@ set -e
 . ../update.sh
 
 # http://www.databasesoup.com/2016/05/changing-postgresql-version-numbering.html
-update_from_base_image "wodby/postgres" "11 10 9.6 9.5 9.4"
+update_from_base_image "wodby/postgres" "12 11 10 9.6 9.5 9.4"
