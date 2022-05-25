42c656e1811378cf856badc85d0c23e63865e965
Chingis Sandanov <chingis@wodby.com>
2022-05-25 22:10:06 +0800

    drop mariadb 10.2 (eol)

--- upstream/mariadb.sh
+++ upstream/mariadb.sh
@@ -4,4 +4,4 @@ set -e
 
 . ../update.sh
 
-update_from_upstream "wodby/mariadb" "10.7 10.6 10.5 10.4 10.3 10.2" "github.com/MariaDB/server"
+update_from_upstream "wodby/mariadb" "10.7 10.6 10.5 10.4 10.3" "github.com/MariaDB/server"
