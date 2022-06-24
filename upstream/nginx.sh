2f7c9e1251216068a6af3535da5b4779febf7c53
Chingis Sandanov <chingis@wodby.com>
2022-06-24 23:26:47 +0800

    Update nginx versions

--- upstream/nginx.sh
+++ upstream/nginx.sh
@@ -4,4 +4,4 @@ set -e
 
 . ../update.sh
 
-update_from_upstream "wodby/nginx" "1.21 1.20 1.19" "github.com/nginx/nginx"
+update_from_upstream "wodby/nginx" "1.23 1.22 1.21 1.20 1.19" "github.com/nginx/nginx"
