62d2c7bb307507dd2f5ad22b673e5e67745c7a86
Chingis Sandanov <chingis@wodby.com>
2022-11-15 21:43:15 +0300

    Upgrade mariadb and alpine versions

--- upstream/mariadb.sh
+++ upstream/mariadb.sh
@@ -4,4 +4,4 @@ set -e
 
 . ../update.sh
 
-update_from_upstream "wodby/mariadb" "10.7 10.6 10.5 10.4 10.3" "github.com/MariaDB/server"
+update_from_upstream "wodby/mariadb" "10.9 10.8 10.7 10.6 10.5 10.4 10.3" "github.com/MariaDB/server"
