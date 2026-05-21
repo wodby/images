7d298e59ed0418477cf302d03e65e9c983f2bf44
Chingis Sandanov <chingis@chingis.io>
2026-05-21 15:57:32 +0300

    Update wp version

--- upstream/wordpress.sh
+++ upstream/wordpress.sh
@@ -4,4 +4,4 @@ set -e
 
 . ../update.sh
 
-update_from_upstream "wodby/wordpress" "6" "github.com/WordPress/WordPress" "4.x"
+update_from_upstream "wodby/wordpress" "7" "github.com/WordPress/WordPress" "4.x"
