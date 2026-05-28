994cac0c64abc125b68fa7f17ee4660e45e0eedb
Chingis Sandanov <chingis@chingis.io>
2026-05-29 00:42:48 +0400

    Remove Elasticsearch and Kibana references and update workflows accordingly

--- upstream/kibana.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-. ../update.sh
-
-update_from_upstream "wodby/kibana" "7" "github.com/elastic/kibana" "" "https://artifacts.elastic.co/downloads/kibana/kibana-{{version}}-linux-x86_64.tar.gz"
